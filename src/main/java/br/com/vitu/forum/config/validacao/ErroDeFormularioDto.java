605984f2e90cf4ec3cb8bf573e3dab9e91e453dd
Victor Couto <victor.vdt1@gmail.com>
2021-06-29 16:51:17 -0300

    feat: add intercept validation

---
+++ src/main/java/br/com/vitu/forum/config/validacao/ErroDeFormularioDto.java
@@ -0,0 +1,15 @@
+package br.com.vitu.forum.config.validacao;
+
+import lombok.Getter;
+
+@Getter
+public class ErroDeFormularioDto {
+
+	private String campo;
+	private String erro;
+	
+	public ErroDeFormularioDto(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+}
