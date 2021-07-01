c950a6b50b24fd8d9495bce02d27edc0dd28130c
Victor Couto <victor.vdt1@gmail.com>
2021-07-01 09:46:01 -0300

    feat: add pagination for method listar

--- src/main/java/br/com/vitu/forum/controller/TopicosController.java
+++ src/main/java/br/com/vitu/forum/controller/TopicosController.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,12 +43,15 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public List<TopicoDto> listar(String nomeCurso){
+	public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,@RequestParam int pagina,@RequestParam int qtd){
+		
+		Pageable pageable = PageRequest.of(pagina, qtd);
+		
 		if(nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(pageable);
 			return TopicoDto.converter(topicos);			
 		}else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, pageable);
 			return TopicoDto.converter(topicos);
 		}
 	}
--- src/main/java/br/com/vitu/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/vitu/forum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.vitu.forum.modelo.Topico;
 import lombok.Getter;
 
@@ -22,7 +24,7 @@ public class TopicoDto {
 		this.dataCriacao = topico.getDataCriacao();
 	}
 
-	public static List<TopicoDto> converter(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDto::new);
 	}
 }
